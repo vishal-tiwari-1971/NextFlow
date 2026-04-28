0ef0813c6b0c0c933e3df9100fa4badeafd85927
vishal tiwari <vikranta1931@gmail.com>
2026-04-28 12:08:21 +0530

    type error

---
+++ types/ffprobe-static.d.ts
@@ -0,0 +1,9 @@
+declare module 'ffprobe-static' {
+  const ffprobePath: string;
+  export default ffprobePath;
+}
+
+declare module 'ffmpeg-static' {
+  const ffmpegPath: string;
+  export default ffmpegPath;
+}
